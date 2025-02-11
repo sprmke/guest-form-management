0c84590c873eae5635b0ea7e97c04f876998cb1f
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-02-12 05:03:52 +0800

    Code cleanup on server directory

--- server/src/routes/guestFormRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { GuestFormController } from '../controllers/guestFormController';
-
-const router = Router();
-
-router.post('/submit-form', GuestFormController.submitForm);
-
-export default router; 
