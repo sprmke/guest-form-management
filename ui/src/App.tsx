a0a0ae99d2b98617c2825a0c65424c385d487e60
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-03-03 18:20:12 +0800

    Improve padding

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,9 +2,9 @@ import { GuestForm } from './components/GuestForm'
 
 function App() {
   return (
-    <main className="min-h-screen p-8 bg-gray-50">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-3xl font-bold mb-8">Guest Form</h1>
+    <main className="px-8 pt-8 pb-16 min-h-screen bg-gray-50">
+      <div className="mx-auto max-w-2xl">
+        <h1 className="mb-8 text-3xl font-bold">Guest Form</h1>
         <GuestForm />
       </div>
     </main>
