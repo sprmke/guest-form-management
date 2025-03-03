9f8a067e399fb5eb73fb077f58f3028deba2ce32
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-03-03 18:23:53 +0800

    Update guest form title

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="px-8 pt-12 pb-24 min-h-screen bg-gray-50">
       <div className="mx-auto max-w-2xl">
-        <h1 className="mb-8 text-3xl font-bold">Guest Form</h1>
+        <h1 className="mb-8 text-3xl font-bold">Kame Home Azure North - Guest Form</h1>
         <GuestForm />
       </div>
     </main>
