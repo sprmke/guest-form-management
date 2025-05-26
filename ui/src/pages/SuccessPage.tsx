552675ff4450140ddfd1c6f983c9851ad3f16262
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-27 01:21:37 +0800

    Reapply "Merge pull request #3 from sprmke/develop"
    This reverts commit d5c0a50786558c9e1639c3277074a09002caa431.

---
+++ ui/src/pages/SuccessPage.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from '@/layouts/MainLayout';
+import { SuccessContent } from '@/components/SuccessContent';
+
+export function SuccessPage() {
+  return (
+    <MainLayout>
+      <SuccessContent />
+    </MainLayout>
+  );
+}
