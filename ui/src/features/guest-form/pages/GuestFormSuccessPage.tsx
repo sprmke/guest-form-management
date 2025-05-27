e57829cb303ce3af3d26e30a9f6b9c59a24c1367
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-28 00:36:42 +0800

    Update guest form success page

---
+++ ui/src/features/guest-form/pages/GuestFormSuccessPage.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from '@/layouts/MainLayout';
+import { GuestFormSuccess } from '@/features/guest-form/components/GuestFormSuccess';
+
+export function GuestFormSuccessPage() {
+  return (
+    <MainLayout>
+      <GuestFormSuccess />
+    </MainLayout>
+  );
+}
