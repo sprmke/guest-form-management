330b25d4beeeaa1276f48b73c7c578b699a77d67
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-28 00:36:20 +0800

    Move guest form default form values

---
+++ ui/src/features/guest-form/constants/guestFormData.ts
@@ -0,0 +1,23 @@
+import { GuestFormData } from "@/features/guest-form/schemas/guestFormSchema";
+import { getDefaultDates, formatDateToYYYYMMDD } from "@/utils/dates";
+
+const { today, tomorrow } = getDefaultDates();
+
+export const defaultFormValues: Partial<GuestFormData> = {
+    guestFacebookName: '',
+    primaryGuestName: '',
+    guestEmail: '',
+    guestPhoneNumber: '',
+    guestAddress: '',
+    checkInDate: formatDateToYYYYMMDD(today),
+    checkOutDate: formatDateToYYYYMMDD(tomorrow),
+    checkInTime: '14:00',
+    checkOutTime: '11:00',
+    nationality: 'Filipino',
+    numberOfAdults: 2,
+    numberOfChildren: 0,
+    guestSpecialRequests: '',
+    findUs: 'Facebook',
+    needParking: false,
+    hasPets: false,
+  };
