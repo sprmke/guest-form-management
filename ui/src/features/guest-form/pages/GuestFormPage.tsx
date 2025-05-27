091652dcfd6ba682393da3fe57b2efb916beaf39
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-28 00:36:35 +0800

    Update guest form page

---
+++ ui/src/features/guest-form/pages/GuestFormPage.tsx
@@ -0,0 +1,10 @@
+import { GuestForm } from '@/features/guest-form/components/GuestForm';
+import { MainLayout } from '@/layouts/MainLayout';
+
+export function GuestFormPage() {
+  return (
+    <MainLayout>
+      <GuestForm />
+    </MainLayout>
+  );
+}
