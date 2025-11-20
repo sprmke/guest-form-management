5f4aa36f648cceafb006b0e8a12e679081aaf508
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-11-21 02:27:02 +0800

    Add sql migration script for add pet type

---
+++ supabase/migrations/20250220000000_add_pet_type.sql
@@ -0,0 +1,7 @@
+-- Add pet_type column to guest_submissions table
+ALTER TABLE guest_submissions
+ADD COLUMN pet_type TEXT;
+
+-- Add comment to explain the column
+COMMENT ON COLUMN guest_submissions.pet_type IS 'Type of pet (e.g., Dog, Cat)';
+
