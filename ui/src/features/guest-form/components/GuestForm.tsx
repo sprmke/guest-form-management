746e60223476a6966936c0edf164b06322a9a163
Michael Manlulu <michaeldmanlulu@gmail.com>
2026-01-13 09:47:35 +0800

    Revert "Fix deployment errors"
    This reverts commit 44f5215dbf576de32a6a2cd7732f8724d91aa50b.

--- ui/src/features/guest-form/components/GuestForm.tsx
+++ ui/src/features/guest-form/components/GuestForm.tsx
@@ -449,7 +449,7 @@ export function GuestForm() {
 
       // Refresh booked dates after cancellation
       await fetchBookedDates();
-
+      
       // Navigate back to the form without bookingId
       navigate('/');
     } catch (error) {
@@ -539,8 +539,7 @@ export function GuestForm() {
           'updateGoogleSheets',
           devApiControls.updateGoogleSheets ? 'true' : 'false'
         );
-        // Only add [TEST] prefix when ?testing=true is explicitly set
-        queryParams.append('testing', isTestingMode ? 'true' : 'false');
+        queryParams.append('testing', 'true');
       } else {
         // In normal production mode, all actions are enabled by default
         queryParams.append('saveToDatabase', 'true');
