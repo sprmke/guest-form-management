d5438ab06a95252528788142aa84df0a660e721f
Michael Manlulu <michaeldmanlulu@gmail.com>
2026-01-14 16:23:47 +0800

    Update index.tsx

--- ui/src/features/guest-form/routes/index.tsx
+++ ui/src/features/guest-form/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Route } from 'react-router-dom';
 import { GuestFormPage } from '@/features/guest-form/pages/GuestFormPage';
 import { GuestFormSuccessPage } from '@/features/guest-form/pages/GuestFormSuccessPage';
+import { CalendarPage } from '@/features/guest-form/pages/CalendarPage';
 
 export const guestFormRoutes = [
   <Route key="guest-form" path="/" element={<GuestFormPage />} />,
@@ -9,4 +10,5 @@ export const guestFormRoutes = [
     path="/success"
     element={<GuestFormSuccessPage />}
   />,
+  <Route key="calendar" path="/calendar" element={<CalendarPage />} />,
 ];
